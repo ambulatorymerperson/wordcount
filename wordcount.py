75fe095d74f95aa5d24ef91dd987965de5446e89
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 12:32:57 -0700

    Initial git commit

---
+++ wordcount.py
@@ -0,0 +1,21 @@
+def count_words(filename):
+    filename = open(filename)
+  
+    word_occurrences = {}
+
+    for line in filename:
+        line = line.rstrip()
+        #remove commas
+        #print words
+        words = line.split(" ")
+
+    #each word in lists of stripped words
+        for word in words:
+            word_occurrences[word] = word_occurrences.get(word, 0) + 1
+
+    for key, value in word_occurrences.items():
+        print "{} {}".format(key, value)
+
+
+
+count_words("test.txt")

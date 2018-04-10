b0a59fe4c0a05dbed14f57c4b39a8b944ffa6463
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 18:01:26 -0700

    Final Version

--- ratings.py
+++ ratings.py
@@ -15,14 +15,17 @@ def restaurant_rating_organizer(filename):
         all_restaurants.append(name_rating)
 
 
-    for restaurant in all_restaurants:
-        restaurants_and_ratings[restaurant[0]] = restaurant[1] 
+    for restaurant, rating in all_restaurants:
+        restaurants_and_ratings[restaurant] = rating 
 
 
-    for restaurant in sorted(restaurants_and_ratings):
-        value = restaurants_and_ratings[restaurant]
+    # for restaurant in sorted(restaurants_and_ratings):
+    #     value = restaurants_and_ratings[restaurant]
 
-        print "{} is rated at {}.".format(restaurant, value)
+    #     print "{} is rated at {}.".format(restaurant, value)
+
+    for restaurant, rating in sorted(restaurants_and_ratings.items()):
+        print "{} is rated at {}.".format(restaurant, rating)
 
  
 restaurant_rating_organizer(filename)
